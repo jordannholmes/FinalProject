884c9a25c96b38e887c5a7d21ee4405acd03a156
jordannholmes <jordannholmes@g.ucla.edu>
2019-06-14 17:14:19 -0700

    FinalProject

---
+++ Vignette/NewData60.R
@@ -0,0 +1,17 @@
+NewData60<-c("Anomobryum auratum",
+           "Artemisia californica",
+           "Bryum argenteum",
+           "Chlorococcum oleofaciens",
+          "Crassula aquatica",
+           "flexuosa",
+           "Gemmabryum dichotomum",
+           "Hormotilopsis gelatinosa",
+           "Interfilum massjukiae",
+           "Pseudochlorella signiensis",
+           "Ptychostomum funkii",
+           "Tetracystis pampae")
+as.data.frame(NewData60)
+
+
+
+
